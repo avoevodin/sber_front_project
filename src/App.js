06749b5a4bc55040f72a81fca81007c1e82fb527
avo888 <avoevodin8888@gmail.com>
2022-03-15 23:35:09 +0300

    added header footer and main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header"
+import Main from "./components/Main/Main"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container py-5">
+      <Header />
+      <hr />
+      <Main />
+      <hr />
+      <Footer />
     </div>
   );
 }
