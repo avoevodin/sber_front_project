0737f8803ca1477910dcb3ffcaf48342f6315a73
avo888 <avoevodin8888@gmail.com>
2022-03-21 18:29:07 +0300

    added posts router for server

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use()
+app.use('/api/v1/phones', )
 
 app.listen(PORT, () => {
     console.log(`The server has been started on port ${PORT}`)
