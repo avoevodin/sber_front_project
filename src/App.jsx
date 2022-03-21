fb094e6ddaac8ab1478df4d15cbc116d12d485c6
avo888 <avoevodin8888@gmail.com>
2022-03-21 17:19:55 +0300

    fixed app ext updated packages with react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./components/Header/Header"
+import Main from "./components/Main/Main"
+import Footer from "./components/Footer/Footer"
+
+function App() {
+  return (
+    <div className="container py-5">
+      
+      <Header />
+      <hr />
+      <Main />
+      <hr />
+      <Footer />
+    </div >
+  );
+}
+
+export default App;
