2a8cd0481c2abefc0e4ee379e0edf523b9038ff3
avo888 <avoevodin8888@gmail.com>
2022-03-21 18:14:04 +0300

    updated react app location

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,17 +2,19 @@ import "./App.css";
 import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container py-5">
-      
+    <BrowserRouter>
       <Header />
-      <hr />
-      <Main />
-      <hr />
+      <div className="container py-5">
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div >
       <Footer />
-    </div >
+    </BrowserRouter>
   );
 }
 
