7ca3c3715ae8582fddddec82ead0a5e7f889ce89
avo888 <avoevodin8888@gmail.com>
2022-04-04 19:51:26 +0300

    added root reducer and init of post reducer

---
+++ app/src/redux/reducers/postsReducer.js
@@ -0,0 +1,13 @@
+/* eslint-disable default-param-last */
+const postsReducer = (store = [], action) => {
+  switch (action.type) {
+    case 'add_post':
+
+      return store
+
+    default:
+      return store
+  }
+}
+
+export default postsReducer
