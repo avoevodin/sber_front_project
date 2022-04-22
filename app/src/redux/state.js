2f75fec4068f4cdbf85d9fec71e7320f1e8a42ec
avo888 <avoevodin8888@gmail.com>
2022-04-22 17:47:44 +0300

    added not working redux structure

--- app/src/redux/state.js
+++ app/src/redux/state.js
@@ -1,6 +1,12 @@
+const user = JSON.parse(localStorage.getItem('user'))
+const authData = user
+  ? { isLoggedIn: true, user }
+  : { isLoggedIn: false, user: null }
+
 const initState = {
   posts: [],
   filter: '',
+  auth: authData,
 }
 
 export default initState
