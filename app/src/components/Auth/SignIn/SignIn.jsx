0e2cca6a8594ff209cca6f7177e7599dcf9b3b70
avo888 <avoevodin8888@gmail.com>
2022-04-22 22:48:34 +0300

    added sign in component with redux

--- app/src/components/Auth/SignIn/SignIn.jsx
+++ app/src/components/Auth/SignIn/SignIn.jsx
@@ -1,31 +1,45 @@
-const SignIn = () => (
-  <div className="d-flex justify-content-center">
-    <form className="d-flex flex-column">
-      <div className="mb-3">
-        <input
-          type="email"
-          className="form-control"
-          aria-describedby="emailHelp"
-          placeholder="Email or username"
-        />
-        <div id="emailHelp" className="form-text">
-          We will never share your email with anyone else.
+import { useDispatch } from 'react-redux'
+import { signInQuery } from '../../../redux/actionCreators/authActionCreators'
+
+const SignIn = () => {
+  const dispatch = useDispatch()
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const formData = Object.fromEntries(new FormData(e.target).entries())
+    dispatch(signInQuery(formData, e))
+  }
+  return (
+    <div className="d-flex justify-content-center">
+      <form onSubmit={submitHandler} className="d-flex flex-column">
+        <div className="mb-3">
+          <input
+            name="username"
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+            placeholder="Email or username"
+          />
+          <div id="emailHelp" className="form-text">
+            We will never share your email with anyone else.
+          </div>
         </div>
-      </div>
-      <div className="mb-3">
-        <input
-          type="password"
-          className="form-control"
-          placeholder="password"
-        />
-      </div>
-      <div className="d-flex justify-content-center">
-        <button type="submit" className="btn btn-primary col-8">
-          Log in
-        </button>
-      </div>
-    </form>
-  </div>
-)
+        <div className="mb-3">
+          <input
+            name="password"
+            type="password"
+            className="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <div className="d-flex justify-content-center">
+          <button type="submit" className="btn btn-primary col-8">
+            Log in
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 export default SignIn
--- app/src/redux/reducers/authReducer.js
+++ app/src/redux/reducers/authReducer.js
@@ -1,9 +1,12 @@
-import { REGISTER_SUCCESS } from '../types/authTypes'
+import { LOGIN_SUCCESS, REGISTER_SUCCESS } from '../types/authTypes'
 
 // eslint-disable-next-line default-param-last
 const authReducer = (store = {}, action) => {
   switch (action.type) {
     case REGISTER_SUCCESS:
+      return action.payload
+
+    case LOGIN_SUCCESS:
       return action.payload
 
     default:
